577ea6cc6b1c72ec34cbef3b4c8bb0affa632a83
richguo <richguo@jetfuel.com.tw>
2022-01-19 15:22:40 +0800

    initial commit

---
+++ create-cluster.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+AWS_PROFILE=menshow_richguo eksctl create cluster \
+--name menshow-dev \
+--region ap-northeast-1 \
+--with-oidc \
+--ssh-access \
+--ssh-public-key menshow-dev-key \
+--nodes 1 \
+--node-type t3.small
